c0e14cdee0b7103f9ba6f4c253687f4c01dc6593
nurbek <nurbek@inbox.ru>
2016-03-31 21:05:37 +0400

    save point

--- blog/website/urls.py
+++ blog/website/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from website import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+        url(r'^reg/$', TemplateView.as_view(template_name='website/reg.html')),
 ]
